10d4ecab336e3c1e53802a2b050b2151da8b83bb
Wasi-Ur-Rahman <wasiurrahman247@gmail.com>
2025-07-07 09:09:31 +0600

    fix: ensure constants are only defined if not already set

--- conversion-tracking.php
+++ conversion-tracking.php
@@ -150,12 +150,24 @@ class WeDevs_WC_Conversion_Tracking {
      * @return void
      */
     public function define_constants() {
-        define( 'WCCT_VERSION', $this->version );
-        define( 'WCCT_FILE', __FILE__ );
-        define( 'WCCT_PATH', dirname( WCCT_FILE ) );
-        define( 'WCCT_INCLUDES', WCCT_PATH . '/includes' );
-        define( 'WCCT_URL', plugins_url( '', WCCT_FILE ) );
-        define( 'WCCT_ASSETS', WCCT_URL . '/assets' );
+        if ( ! defined( 'WCCT_VERSION' ) ) {
+            define( 'WCCT_VERSION', $this->version );
+        }
+        if ( ! defined( 'WCCT_FILE' ) ) {
+            define( 'WCCT_FILE', __FILE__ );
+        }
+        if ( ! defined( 'WCCT_PATH' ) ) {
+            define( 'WCCT_PATH', dirname( WCCT_FILE ) );
+        }
+        if ( ! defined( 'WCCT_INCLUDES' ) ) {
+            define( 'WCCT_INCLUDES', WCCT_PATH . '/includes' );
+        }
+        if ( ! defined( 'WCCT_URL' ) ) {
+            define( 'WCCT_URL', plugins_url( '', WCCT_FILE ) );
+        }
+        if ( ! defined( 'WCCT_ASSETS' ) ) {
+            define( 'WCCT_ASSETS', WCCT_URL . '/assets' );
+        }
     }
 
     /**
